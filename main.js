32383f83e1ab3f54fbd3c8e40bf3f527aa5a4a70
andreymaslovru <74611548+andreymaslovru@users.noreply.github.com>
2021-04-16 15:37:42 +0700

    Add files via upload
    added function delete item__list

--- main.js
+++ main.js
@@ -4,13 +4,25 @@ const App = {
             counter: 0,
             listTitle: "Список задач",
             valueInput: '',
+            sendingVal: [],
         }
     },
 
     methods: {
         getValueInput(event) { 
             this.valueInput = event.target.value;
-            console.log(event.target.value);
+        },
+        sendList() {
+            this.sendingVal.push(this.valueInput);
+            this.valueInput = '';
+        },
+        deleteList(index) {
+            this.sendingVal.splice(index, 1);
+        },
+        listenPressKey(event) {
+            if (event.key === "Enter") {
+                this.sendList();
+            }
         }
     }
 }
